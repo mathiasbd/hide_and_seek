66e6423028a6ab1198167c78f4604799ebb96785
s235257 <Gabi04@live.dk>
2024-06-22 17:09:34 +0200

    sadas

--- hide_and_seek/lib/classes/abillity_manager.dart
+++ hide_and_seek/lib/classes/abillity_manager.dart
@@ -5,29 +5,11 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 
 class AbillityManager {
 
-    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
-
-
   void getHiderLocations() {
     // Make a LatLng field in the User class
     // constantly update the user's location in the database
     // Get the hider locations from the database
     // make points on the map for each hider
-  }
-
-  void getNoiseQue() {
-    await _firebaseMessaging.send(RemoteMessage(
-      notification: Notification(
-        title: 'Noise Que Notification',
-        body: 'A noise que has been triggered',
-      ),
-      data: {
-        'click_action': 'FLUTTER_NOTIFICATION_CLICK',
-        'id': '1',
-        'status': 'done',
-      },
-      topic: 'noiseQue',
-    ));
   }
 
   bool catchHider(LatLng hiderLocation, LatLng seekerLocation, double radius) {
