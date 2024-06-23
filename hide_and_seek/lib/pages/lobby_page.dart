a89f136defed37f3ec369e9d23f4ef79738a91aa
mathiasbd <s235270@dtu.dk>
2024-06-23 14:01:28 +0200

    lobby page fix

--- hide_and_seek/lib/pages/lobby_page.dart
+++ hide_and_seek/lib/pages/lobby_page.dart
@@ -79,30 +79,8 @@ class Lobby extends StatelessWidget {
         var participants =
             List<Map<String, dynamic>>.from(matchData['participants'] ?? []);
         var gameStarted = matchData['Match started'];
-        if (gameStarted && navigated == false) {
-          navigated = true;
-          if (user.role == 'Seeker') {
-            debugPrint('Changed page to seeker_page');
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        SeekerPage(matchName: matchName, user: user)),
-              );
-            });
-          } else if(user.role == 'Hider') {
-            debugPrint('Changed page to hider_page');
-            WidgetsBinding.instance.addPostFrameCallback((_) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                    builder: (context) =>
-                        HiderPage(matchName: matchName, user: user)),
-              );
-            });
-          }
-        }
+        checkGameStarted(gameStarted, navigated, context, firestoreController);
+        
         if (participants.isEmpty) {
           return const Center(child: Text('No participants found'));
         } else {
@@ -205,10 +183,35 @@ class Lobby extends StatelessWidget {
     bool isReady = await firestoreController.checkUsersReady(matchName, user);
     if (isReady) {
       await firestoreController.findRandomSeeker(matchName);
-      String updatedRole =
-          await firestoreController.fetchUserRole(matchName, user);
-      user.role = updatedRole;
       await firestoreController.changeGameStarted(matchName);
     }
+  }
+  Future<void> checkGameStarted(gameStarted, navigated, context, firestoreController) async {
+    if (gameStarted && navigated == false) {
+          String updatedRole = await firestoreController.fetchUserRole(matchName, user);
+          user.role = updatedRole;
+          navigated = true;
+          if (user.role == 'Seeker') {
+            debugPrint('Changed page to seeker_page');
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        SeekerPage(matchName: matchName, user: user)),
+              );
+            });
+          } else if(user.role == 'Hider') {
+            debugPrint('Changed page to hider_page');
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) =>
+                        HiderPage(matchName: matchName, user: user)),
+              );
+            });
+          }
+        }
   }
 }
